d6719bdff13be5494c47d61dfd89000322dd399e
Đào Mỹ Đông <daomydong1995@gmail.com>
2017-05-05 23:54:47 +0700

    Màn hình sflash + main

---
+++ app/src/main/java/com/example/daomy/fitnesstutorial3/SplashScreenActivity.java
@@ -0,0 +1,72 @@
+package com.example.daomy.fitnesstutorial3;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import butterknife.BindView;
+
+
+/**
+ * Created by daomy on 5/5/2017.
+ */
+
+public class SplashScreenActivity extends AppCompatActivity {
+    protected boolean _active = true;
+    protected int _splashTime = 5000;
+
+
+    /**
+     * Called when the activity is first created.
+     */
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_sceen);
+
+        // thread for displaying the SplashScreen
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if (_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch (InterruptedException e) {
+
+                } finally {
+                    finish();
+                    Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    SplashScreenActivity.this.startActivity(mainIntent);
+                    SplashScreenActivity.this.finish();
+                }
+            }
+        };
+        splashTread.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            _active = false;
+        }
+        return true;
+    }
+}
+
+
